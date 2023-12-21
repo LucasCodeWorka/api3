0deb10d0e8345cf6935ba20221a9433032f273a3
Lucas Angelo <lucasangelo@MacBook-Air-de-Lucas.local>
2023-12-21 17:01:48 -0300

    feat:api3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const pool = require('./db');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Exemplo de rota para consulta no banco de dados
 app.get('/consultar-dados', async (req, res) => {
